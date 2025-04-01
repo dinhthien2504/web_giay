29382587b57e99b152449041a4d34ee324ba3f5b
Nguyễn Đức Huy <nguyenduchuy@Hyu.lan>
2025-04-01 16:01:32 +0700

    ok

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -18,7 +18,7 @@ Route::post('/login', [AuthenticatedSessionController::class, 'store'])
 
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
     ->middleware('guest')
-    ->name('email');
+    ->name('password.email');
 
 Route::post('/reset-password', [NewPasswordController::class, 'store'])
     ->middleware('guest')
