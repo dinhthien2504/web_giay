0b529417ade2091d02fecd5f10ea3ac73c0624c9
Nguyễn Đức Huy <nguyenduchuy@Hyu.lan>
2025-04-03 01:21:05 +0700

    xong dang ky

--- backend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ backend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,6 +36,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return response()->json(['message' => 'Tạo tài khoản thành công!', 'user' => $user, 'role' => 0], 201);
+        return response()->json(['message' => 'Tạo tài khoản thành công!', 'user' => $user, 'role' => 0, 'status'=> 200], 200);
     }
 }
--- frontend/src/pages/client/auth/register.vue
+++ frontend/src/pages/client/auth/register.vue
@@ -1,116 +1,150 @@
-    <template>
-        <div class="container-form ">
-            <div class="form__container">
-                <div class="text-center">
-                    <img src="/img/logo.jpg" class="brand-logo mb-3 text-center" alt="Logo ShoeStore">
-                </div>
-                <h2 class="mb-3 text-uppercase text-primary text-center">Đăng Ký</h2>
-                <p v-if="message" class="alert alert-success">{{ message }}</p>
-                <form @submit.prevent="submitRegister">
-                    <div class="mb-3">
-                        <input type="text" v-model="user.name" class="form-control" placeholder="Họ và Tên">
-                        <p class="text-danger" v-if="errors.name">{{ Array.isArray(errors.name) ?
-                            errors.name[0] : errors.name }}</p>
-                    </div>
-                    <div class="mb-3">
-                        <input type="email" v-model="user.email" class="form-control" placeholder="Email">
-                        <p class="text-danger" v-if="errors.email">{{ Array.isArray(errors.email) ?
-                            errors.email[0] : errors.email }}</p>
-                    </div>
-                    <div class="mb-3">
-                        <input type="password" v-model="user.password" class="form-control" placeholder="Mật khẩu">
-                        <p class="text-danger" v-if="errors.password">{{ Array.isArray(errors.password) ?
-                            errors.password[0] : errors.password }}</p>
-                    </div>
-                    <div class="mb-3">
-                        <input type="password" v-model="user.password_confirmation" class="form-control" placeholder="Mật khẩu">
-                        <p class="text-danger" v-if="errors.password">{{ Array.isArray(errors.password) ?
-                            errors.password[0] : errors.password }}</p>
-                    </div>
-                    <button type="submit" class="btn btn-primary w-100">Đăng Ký</button>
-                    <div class="mt-2 text-center">
-                        <span>Đã có tài khoản? <router-link to="/dang-nhap" class="text-primary ">Đăng
-                                nhập</router-link></span>
-                    </div>
-                </form>
-            </div>
+<template>
+  <div class="container-form">
+    <div class="form__container">
+      <div class="text-center">
+        <img
+          src="/img/logo.jpg"
+          class="brand-logo mb-3 text-center"
+          alt="Logo ShoeStore"
+        />
+      </div>
+      <h2 class="mb-3 text-uppercase text-primary text-center">Đăng Ký</h2>
+      <p v-if="message" class="alert alert-success">{{ message }}</p>
+      <p v-if="errorsMessage" class="alert alert-danger">{{ errorsMessage }}</p>
+      <form @submit.prevent="submitRegister">
+        <div class="mb-3">
+          <input
+            type="text"
+            v-model="user.name"
+            class="form-control"
+            placeholder="Họ và Tên"
+          />
+          <p class="text-danger" v-if="errors.name">
+            {{ Array.isArray(errors.name) ? errors.name[0] : errors.name }}
+          </p>
         </div>
-    </template>
+        <div class="mb-3">
+          <input
+            type="email"
+            v-model="user.email"
+            class="form-control"
+            placeholder="Email"
+          />
+          <p class="text-danger" v-if="errors.email">
+            {{ Array.isArray(errors.email) ? errors.email[0] : errors.email }}
+          </p>
+        </div>
+        <div class="mb-3">
+          <input
+            type="password"
+            v-model="user.password"
+            class="form-control"
+            placeholder="Mật khẩu"
+          />
+          <p class="text-danger" v-if="errors.password">
+            {{
+              Array.isArray(errors.password)
+                ? errors.password[0]
+                : errors.password
+            }}
+          </p>
+        </div>
+        <div class="mb-3">
+          <input
+            type="password"
+            v-model="user.password_confirmation"
+            class="form-control"
+            placeholder="Xac nhan mật khẩu"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary w-100">Đăng Ký</button>
+        <div class="mt-2 text-center">
+          <span
+            >Đã có tài khoản?
+            <router-link to="/dang-nhap" class="text-primary"
+              >Đăng nhập</router-link
+            ></span
+          >
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
 
-    <script>    
-    import { ref } from 'vue';
-    import axios from 'axios';
-    import { urlApi } from '../../../components/store';
-    export default {
-        setup() {
-            const errors = ref({});
-            const message = ref('');
-            const user = ref({
-                name: '',
-                email: '',
-                password: '',
-                password_confirmation: ''
-            });
-            const submitRegister = () => {
-                //Check lỗi bên frontend
-                errors.value = {};
-                message.value = '';
-                // Kiểm tra họ tên
-                if (!user.value.name.trim()) {
-                    errors.value.name = 'Trường tên không được để trống.';
-                } else if (user.value.name.length < 3) {
-                    errors.value.name = 'Trường tên phải có ít nhất 3 ký tự';
-                }
+<script>
+import { ref } from "vue";
+import axios from "axios";
+import { urlApi } from "../../../components/store";
+export default {
+  setup() {
+    const errors = ref({});
+    const message = ref("");
+    const errorsMessage = ref("");
+    const user = ref({
+      name: "",
+      email: "",
+      password: "",
+      password_confirmation: "",
+    });
+    const submitRegister = () => {
+      //Check lỗi bên frontend
+      errors.value = {};
+      message.value = "";
+      // Kiểm tra họ tên
+      if (!user.value.name.trim()) {
+        errors.value.name = "Trường tên không được để trống.";
+      } else if (user.value.name.length < 3) {
+        errors.value.name = "Trường tên phải có ít nhất 3 ký tự";
+      }
 
-                // Kiểm tra email
-                if (!user.value.email.trim()) {
-                    errors.value.email = 'Trường địa chỉ email không được để trống.';
-                } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user.value.email)) {
-                    errors.value.email = 'Email bạn nhập vào không hợp lệ.';
-                }
+      // Kiểm tra email
+      if (!user.value.email.trim()) {
+        errors.value.email = "Trường địa chỉ email không được để trống.";
+      } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user.value.email)) {
+        errors.value.email = "Email bạn nhập vào không hợp lệ.";
+      }
 
-                // Kiểm tra mật khẩu
-                if (!user.value.password.trim()) {
-                    errors.value.password = 'Trường mật khẩu không được để trống.';
-                } else if (user.value.password.length < 6) {
-                    errors.value.password = 'Mật khẩu bạn nhập phải lớn hơn 6 ký tự.';
-                }
-                // Nếu có lỗi thì không gửi request
-                if (Object.keys(errors.value).length > 0) {
-                    return;
-                }
-                //Nếu không có lỗi gì thì gửi lên server
-                register(user.value)
-            }
-            const register = async (user) => {
-                try {
-                    errors.value = {}; // Reset lỗi cũ
-                    const response = await axios.post(`${urlApi}/register`, user);
-
-                    // Nếu đăng ký thành công
-                    message.value = response.data.message;
-                    user.name = '';
-                    user.email = '';
-                    user.password = '';
-
-                } catch (error) {
-                    message.value = error.response.data.message;
-                    
-                    if (error.response && error.response.data.errors) {
-                        errors.value = error.response.data.errors;
-                    } else {
-                        message.value = "Lỗi kết nối đến server!";
-                    }
-                }
-            };
-
-            return {
-                user,
-                message,
-                errors,
-                register,
-                submitRegister
-            };
-        }
+      // Kiểm tra mật khẩu
+      if (!user.value.password.trim()) {
+        errors.value.password = "Trường mật khẩu không được để trống.";
+      } else if (user.value.password.length < 6) {
+        errors.value.password = "Mật khẩu bạn nhập phải lớn hơn 6 ký tự.";
+      }
+      // Nếu có lỗi thì không gửi request
+      if (Object.keys(errors.value).length > 0) {
+        return;
+      }
+      //Nếu không có lỗi gì thì gửi lên server
+      register(user.value);
     };
-    </script>
+    const register = async (user) => {
+      try {
+        errors.value = {}; // Reset lỗi cũ
+        const response = await axios.post(`${urlApi}/register`, user);
+
+        if (response.status === 200) {
+          message.value = response.data.message;
+        }else if (response.status === 422) {
+          errorsMessage.value = response.data.message;
+        }
+        
+      } catch (error) {
+        if (error.response && error.response.data.errors) {
+          errors.value = error.response.data.errors;
+        } else {
+          message.value = "Lỗi kết nối đến server!";
+        }
+      }
+    };
+
+    return {
+      user,
+      message,
+      errors,
+      register,
+      submitRegister,
+      errorsMessage,
+    };
+  },
+};
+</script>
